45646d3b29ba88db6bc58008dc69a04bb3955dd3
makoto abe <abe00makoto@gmail.com>
2017-07-18 20:23:48 +0900

    wip

--- router_test/src/components/one.tsx
+++ router_test/src/components/one.tsx
@@ -5,18 +5,19 @@ interface AppProps extends React.Props<State> {
     test: string;
 }
 
-const mapStateToProps = (state: State, ownProps: AppProps): AppProps => ({
-    test: state.test
+const mapStateToProps = (state: State, ownProps: {}): AppProps => ({
+    test: state.test,
+    ...ownProps
 });
 
-// const App = (props: AppProps & DispatchProp<{}>) => {
-//     props.dispatch({ type: 'test' });
-//     return (<h1> App test </h1>);
-// };
-class App extends React.Component<AppProps & DispatchProp<{}>, {}> {
-    render() {
-        this.props.dispatch({ type: 'test' });
-        return (<h1> App test {`${JSON.stringify(this.props)}`} </h1>);
-    }
-}
+const App = (props: AppProps & DispatchProp<{}>) => {
+    props.dispatch({ type: 'test' });
+    return (<h1> App test </h1>);
+};
+// class App extends React.Component<AppProps & DispatchProp<{}>, {}> {
+//     render() {
+//         this.props.dispatch({ type: 'test' });
+//         return (<h1> App test {`${JSON.stringify(this.props)}`} </h1>);
+//     }
+// }
 export default connect(mapStateToProps)(App);
--- router_test/src/index.tsx
+++ router_test/src/index.tsx
@@ -2,15 +2,15 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import RootReducer from './reducers';
 
 let store = createStore(RootReducer);
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root') as HTMLElement
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
